014e43462ce2f2d093f8857e9c8b182288a91123
Camvonn <145286014+Camvonn@users.noreply.github.com>
2024-08-20 10:18:18 -0400

    Add files via upload

---
+++ card.js
@@ -0,0 +1,14 @@
+// Created by Guilherme Oliveira - 05/15/2024
+// might change to make all info string and use indexes to access each piece of information
+
+class Card {
+    constructor(number, color, shape, attr, image){
+        this.number = number;
+        this.color = color;
+        this.shape = shape;
+        this.attr = attr;
+        this.image = image;
+    }
+}
+
+export default Card;
